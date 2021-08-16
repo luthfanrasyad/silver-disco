58b7d2ba6f6b81d1b1affecdd0ec27775f720f53
Luthfan Rasyad Maulana <luthfan.maulana@ovo.id>
2021-08-16 18:08:41 +0700

    add skeleton loader

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,19 @@
 <template lang='pug'>
 div
   div(style='height: 35px')
-  .center(v-if='!isLoading')
-    .tag-container 
-      span.tag--active(v-for='tag in activeTags', style='background-color: rgba(6, 89, 121, 0.8)', @click='toggleTag(tag)') {{ tag }}
+  .center
+    .tag-container(v-if='isLoading')
+      .placeholder-tag(v-for='i in 3')
+    .tag-container(v-else)
+      span.tag--active(
+        v-for='tag in activeTags',
+        style='background-color: rgba(6, 89, 121, 0.8)',
+        @click='toggleTag(tag)'
+      ) {{ tag }}
       span.tag--inactive(v-for='tag in inactiveTags', @click='toggleTag(tag)') {{ tag }}
-    .card-container
+    .card-container(v-if='isLoading')
+      .placeholder-card(v-for='i in 3')
+    .card-container(v-else)
       .card(
         v-for='(item, index) in allFood',
         :style='color()',
@@ -13,7 +21,6 @@ div
         @click='$router.push(`/recipes/${item.slug}`)'
       )
         h2 {{ item.name }}
-  div(v-else) Loading
 </template>
 
 <script>
@@ -127,7 +134,68 @@ export default {
   color: white;
   border: none;
 }
-.tag--inactive {
+
+.placeholder-card {
+  background-color: rgb(219, 219, 219);
+  box-shadow: 0 4px 10px 0 rgb(231, 231, 231);
+  border-radius: 5px;
+  margin: 5px;
+  height: 160px;
+  width: 160px;
+  position: relative;
+  overflow: hidden;
+}
+.placeholder-card::before {
+  content: '';
+  display: block;
+  position: absolute;
+  left: -150px;
+  top: 0;
+  height: 100%;
+  border-radius: 15px;
+  width: 160px;
+  background: linear-gradient(to right, transparent 0%, #eeeded 50%, transparent 100%);
+  animation: load 1s cubic-bezier(0.4, 0, 0.2, 1) infinite;
+}
+.placeholder-tag {
+  background-color: rgb(219, 219, 219);
+  box-shadow: 0 4px 10px 0 rgb(231, 231, 231);
+  border-radius: 15px;
+  padding: 4px 12px;
+  margin-right: 5px;
+  margin-top: 5px;
+  height: 30px;
+  width: 60px;
+  position: relative;
+  overflow: hidden;
+}
+.placeholder-tag::before {
+  content: '';
+  display: block;
+  position: absolute;
+  left: -60px;
+  top: 0;
+  height: 100%;
+  border-radius: 15px;
+  width: 60px;
+  background: linear-gradient(to right, transparent 0%, #eeeded 50%, transparent 100%);
+  animation: load-tag 1s cubic-bezier(0.4, 0, 0.2, 1) infinite;
+}
+@keyframes load {
+  from {
+    left: -150px;
+  }
+  to {
+    left: 100%;
+  }
+}
+@keyframes load-tag {
+  from {
+    left: -60px;
+  }
+  to {
+    left: 100%;
+  }
 }
 @media only screen and (min-width: 143px) {
   .card-container {
